184b6ca39c6a64d736d184807eff8073b501dec8
CaiJunMao <windgream@outlook.com>
2019-05-22 16:52:44 +0800

    初始化vue项目，添加了vuex, router,axios, element ui

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import './plugins/axios'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-
+//import './plugins/element.js'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   store,
   router,
